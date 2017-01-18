a778257138c3de22dec625626cc43ad73a0b6984
Quentin <kentdu38@gmail.com>
2017-01-18 10:41:41 +0100

    wireframe

--- js/3d.js
+++ js/3d.js
@@ -9,6 +9,9 @@ var camera, controls, scene, renderer;
 var lighting, ambient, keyLight, fillLight, backLight;
 var windowHalfX;
 var windowHalfY;
+var obj;
+var id = 0;
+var objMaterials = {};
 
 init();
 animate();
@@ -62,11 +65,19 @@ function init() {
 		objLoader.setPath( 'assets/male02/' );
 		objLoader.load( 'male02.obj', function ( object ) {
 			object.position.y = - 95;
-			scene.add( object );
+			obj = object;
+			scene.add( obj );
 		}, onProgress, onError );
 	});
 
 
+	objMaterials[0] = obj.materials;
+	objMaterials[1] = new THREE.MeshLambertMaterial( { color: 0xdddddd, shading: THREE.SmoothShading }  ); 
+	objMaterials[2] = new THREE.MeshPhongMaterial( { color: 0xFF0000, specular: 0x009900, shininess: 30, shading: THREE.FlatShading }   ); 
+	objMaterials[3] = new THREE.MeshBasicMaterial( { color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending }  );
+	objMaterials[4] = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe : true  }  );
+
+	
 
 	// Render
 	renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -103,6 +114,15 @@ function onKeyboardEvent(e) {
 			scene.add(keyLight);
 			scene.add(fillLight);
 			scene.add(backLight);
+		}
+		else if(e.code === 'KeyT'){
+			if(id < 5){
+				changeMaterial(id);
+				id++;
+			}
+			else if(id == 5)
+				id = 0;
+				changeMaterial(id);
 		} else {
 			ambient.intensity = 1.0;
 			scene.remove(keyLight);
@@ -118,4 +138,9 @@ function animate() {
 }
 function render() {
 	renderer.render(scene, camera);
+}
+
+
+function changeMaterial(id){
+    obj.materials = materials[id];
 }
